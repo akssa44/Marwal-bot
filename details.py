0449381e14f3306255c7607a132f90961d93cb9a
altafpathan65012 <137278745+altafpathan65012@users.noreply.github.com>
2024-06-02 20:03:32 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,5 +1,5 @@
-api_id = 29410389
-api_hash = "0c716764715886f6641477ffbb63e1ee"
-bot_token = "5946934925:AAGoALZ3-Cr8hQaBdzHhmW3pknt-h1LVVpI"
+api_id = 23031620
+api_hash = "31cb00c1cbe580394778b43105864bca"
+bot_token = "6749533828:AAGUsdTyHDaeA24xCRa32Z5eH82sUWzhLjs"
 sudo_groups = -1001777454744
 
